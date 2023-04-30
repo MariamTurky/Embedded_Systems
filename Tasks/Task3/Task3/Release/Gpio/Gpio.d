86a7fbe937e43c818049de89ec558414abbc5ba0
MariamTurky <93421790+MariamTurky@users.noreply.github.com>
2023-04-30 19:11:24 +0200

    Add files via upload

---
+++ Tasks/Task3/Task3/Release/Gpio/Gpio.d
@@ -0,0 +1,10 @@
+Gpio/Gpio.o: ../Gpio/Gpio.c ../Gpio/Gpio.h \
+ C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib/Std_Types.h \
+ ../Gpio/Gpio_Private.h \
+ C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib/Utils.h \
+ C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib/Std_Types.h
+../Gpio/Gpio.h:
+C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib/Std_Types.h:
+../Gpio/Gpio_Private.h:
+C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib/Utils.h:
+C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib/Std_Types.h:
---
+++ Tasks/Task3/Task3/Release/Gpio/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Gpio/Gpio.c 
+
+OBJS += \
+./Gpio/Gpio.o 
+
+C_DEPS += \
+./Gpio/Gpio.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Gpio/%.o: ../Gpio/%.c Gpio/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra -g -DNDEBUG -DSTM32F401xE -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -I"C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib" -I"C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Rcc" -I"C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Gpio" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
