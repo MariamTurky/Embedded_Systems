3f71ff12efe415892cb291155aa97f0842cb7a19
MariamTurky <93421790+MariamTurky@users.noreply.github.com>
2023-04-30 19:14:21 +0200

    Add files via upload

---
+++ Tasks/Task3/Release/src/main.d
@@ -0,0 +1,7 @@
+src/main.o: ../src/main.c \
+ C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Gpio/Gpio.h \
+ C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib/Std_Types.h \
+ C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Rcc/Rcc.h
+C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Gpio/Gpio.h:
+C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Lib/Std_Types.h:
+C:\Users\dell\eclipse-workspace\Lab4_Startup_Project_TODO\Rcc/Rcc.h:
